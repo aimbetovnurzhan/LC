6f1222de09f9c5aeb71c6655a1ad7f81a254e74b
Nurzhan <aymbetovnurzhan@gmail.com>
2026-03-30 23:57:34 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3861-minimum-capacity-box/3861-minimum-capacity-box.java
+++ 3861-minimum-capacity-box/3861-minimum-capacity-box.java
@@ -1,14 +1,20 @@
 class Solution {
     public int minimumIndex(int[] capacity, int itemSize) {
-        int minDifInd = -1, n = capacity.length;
+        int minDifInd = -1, n = capacity.length, prev = 101;
         for (int i = 0; i < n; i ++) {
-            int dif = capacity[i] - itemSize;
-            if (dif < 0) continue;
-            else if (dif == 0) return i; 
-            else {
-                if (minDifInd < 0) minDifInd = i;
-                else if (dif < (capacity[minDifInd] - itemSize)) {
-                    minDifInd = i;
+            int curr = capacity[i];
+            if (curr < prev) {
+                int dif = curr - itemSize;
+                if (dif < 0) continue;
+                else if (dif == 0) return i; 
+                else {
+                    if (minDifInd < 0) {
+                        minDifInd = i;
+                        prev = curr;
+                    } else if (dif < (prev - itemSize)) {
+                        minDifInd = i;
+                        prev = curr;
+                    }
                 }
             }
         }
