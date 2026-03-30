e86b7f717ae7d577809e803c8269db7643750585
Nurzhan <aymbetovnurzhan@gmail.com>
2026-03-30 23:14:42 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3866-first-unique-even-element/3866-first-unique-even-element.java
+++ 3866-first-unique-even-element/3866-first-unique-even-element.java
@@ -1,16 +1,18 @@
 class Solution {
     public int firstUniqueEven(int[] nums) {
-        for (int i = 0; i < nums.length; i ++) {
-            if (nums[i] % 2 == 0 && nums[i] != 0) {
+        int n = nums.length;
+        for (int i = 0; i < n; i ++) {
+            int target = nums[i];
+            if (target % 2 == 0 && target != 0) {
                 int cntr = 0;
-                for (int j = i + 1; j < nums.length; j++) {
+                for (int j = i + 1; j < n; j++) {
                     if (nums[j] % 2 != 0) nums[j] = 0;
-                    else if (nums[j] == nums[i]) {
+                    else if (nums[j] == target) {
                         nums[j] = 0;
                         cntr ++;
                     }
                 }
-                if (cntr == 0) return nums[i];
+                if (cntr == 0) return target;
             }
         }
         return -1;
