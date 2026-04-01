704fe34bb17a5bcd7a821a5372bdf11aebca67ef
Nurzhan <aymbetovnurzhan@gmail.com>
2026-04-01 22:38:08 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3842-toggle-light-bulbs/3842-toggle-light-bulbs.java
+++ 3842-toggle-light-bulbs/3842-toggle-light-bulbs.java
@@ -3,11 +3,7 @@ class Solution {
         var res = new ArrayList<Integer>();
         var temp = new int[101];
         for (int i: bulbs) {
-            if (temp[i] == 0) {
-                temp[i] = 1;
-            } else {
-                temp[i] = 0;
-            }
+            temp[i] ^= 1;
         }
         for (int i = 1; i <= 100; i++) {
             if (temp[i] == 1) {
