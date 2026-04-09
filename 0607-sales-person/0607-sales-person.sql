33f48ae8ec1087268066e641b2ced09f329c51ed
Nurzhan <aymbetovnurzhan@gmail.com>
2026-04-10 01:09:52 +0500

    Time: 347 ms (39.63%), Space: 0B (100%) - LeetHub

---
+++ 0607-sales-person/0607-sales-person.sql
@@ -0,0 +1,14 @@
+-- Write your PostgreSQL query statement below
+select
+    name
+from
+    SalesPerson
+where
+    sales_id not in (
+        select
+            sales_id
+        from
+            Orders o
+                left join Company c on c.com_id = o.com_id
+        where
+            c.name = 'RED')
